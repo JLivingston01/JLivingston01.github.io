22cc12ee89f9c667c4611dd78bcf18cc154f27e9
JasonLivingstonColumbia <33710575+JasonLivingstonColumbia@users.noreply.github.com>
2019-01-28 22:54:30 -0500

    edits

--- projects/hill_fit.py
+++ projects/hill_fit.py
@@ -1,25 +1,17 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 23 11:42:41 2019
-
-@author: jliv
-"""
 
 import pandas as pd
 import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("path/to/file/company_data.csv")
+data = pd.read_csv("path/to/data/company_data.csv")
 
 data['c1_cume'] = data['c1'].cumsum()
 data['c2_cume'] = data['c2'].cumsum()
 
-
 def HILL(x,k,s,c):
     h = c/(1+(x/k)**(-s))
     return h
-
 
 def normalize(d):
     d = np.array(d)
@@ -39,22 +31,14 @@ def de_normalize(dnorm,min_norm,max_norm):
 #Updates estimates of k,s,c using learning rate a and the partial derivative of k,s,c
 
 def HILL_fit(d,epoch = 200, a = .1,init = 0, s = 1.9, k_perc = 2, c_init = 1.3):
-    y = d
-    k = len(d)/k_perc
-    s = s
-    c = c_init
+    y,k,s,c = d,len(d)/k_perc,s,c_init
     ynorm,min_norm,max_norm = normalize(y)
     for j in range(epoch):
         for i in range(1+init,len(ynorm)+1+init):
-            h = HILL(i,k,s,c)
-            grad_k = c*((1/((1+(i/k)**(-s))**(2)))*(s)*(i/k)**((-s-1)))*(-i)/(k**2)
-            grad_s = c*(1/((1+(i/k)**(-s))**2))*math.log(i/k)*(i/k)**(-s)
-            grad_c = 1/((1+(i/k)**(-s)))
-            e = ynorm[i-1-init] - h
-            k = k + a*3*grad_k*e
-            s = s + a*grad_s*e
-            c = c + a*grad_c*e
-    
+            k,s,c = k + a*(c*((1/((1+(i/k)**(-s))**(2)))*(s)*(i/k)**((-s-1)))*(-i)/(k**2))*(ynorm[i-1-init] - HILL(i,k,s,c)),\
+                    s + a*(c*(1/((1+(i/k)**(-s))**2))*math.log(i/k)*(i/k)**(-s))*(ynorm[i-1-init] - HILL(i,k,s,c)),\
+                    c + a*(1/((1+(i/k)**(-s))))*(ynorm[i-1-init] - HILL(i,k,s,c))
+
     fit = []
     for i in range(1+init,len(d)+1+init):
         h_pred = HILL(i,k,s,c)
@@ -63,7 +47,7 @@ def HILL_fit(d,epoch = 200, a = .1,init = 0, s = 1.9, k_perc = 2, c_init = 1.3):
     return fit,k,s,c,min_norm,max_norm
 
 
-def hill_proj(k,s,p, c,init = 50):
+def hill_proj(k,s,p, c,init = 0):
     k = k
     s = s
     projection = []
@@ -72,17 +56,53 @@ def hill_proj(k,s,p, c,init = 50):
         projection.append(h)
     return(projection)
 
-hillfit,k,s,c,min_norm,max_norm = HILL_fit(d = data['c2_cume'],epoch = 2000,init = 25, s = 1.3,k_perc = 1)
+hillfit,k,s,c,min_norm,max_norm = HILL_fit(d = data['c2_cume'],epoch = 12000,init = 0, s = 10,k_perc = 3)
 plt.plot(de_normalize(hillfit,min_norm,max_norm))
 plt.plot(data['c2_cume'])
 
 
-hillfit,k,s,c,min_norm,max_norm = HILL_fit(d = data['c1_cume'],epoch = 2000,init = 25, s = 1.3,k_perc = 1)
+hillfit,k,s,c,min_norm,max_norm = HILL_fit(d = data['c1_cume'],epoch = 12000,a = 1,init = 0, s = 17,k_perc = 1)
+plt.figure(figsize = (8,6))
+plt.plot(de_normalize(hillfit,min_norm,max_norm), label='Hill Fit')
+plt.plot(data['c1_cume'], label='Cumulative KPI')
+plt.title("The Fit of Hill Closely Resembles the KPI Data")
+plt.legend(loc = 2)
+plt.ylabel("Cumulative KPI")
+plt.xlabel("Time (Months)")
+plt.show()
+
+#plt.plot((de_normalize(hillfit,min_norm,max_norm) - data['c1_cume'])/data['c1_cume'])
+mag_error = (de_normalize(hillfit,min_norm,max_norm) - data['c1_cume'])
+plt.figure(figsize = (8,6))
+plt.plot(mag_error)
+plt.ylabel("Magnitude of Model Error")
+plt.xlabel("Time (Months)")
+plt.title("Model Error Shows Normal Periodicity, Well Centered Near 0")
+plt.show()
+
+              
+proj_hil = de_normalize(hill_proj(k,s,[25,50,75,100,125,150,170,200,225,250,275,300],init = 0, c=c),min_norm,max_norm)
+plt.figure(figsize = (8,6))
+plt.scatter([25,50,75,100,125,150,170,200,225,250,275,300],proj_hil, label = 'Projection using Hill')
+plt.plot(data['c1_cume'], label = 'Cumulative KPI 1')
+plt.ylabel("Cumulative KPI")
+plt.xlabel("Time (Months)")
+plt.legend(loc = 2)
+plt.title("This Hill Projection May Indicate the Potential for Further Scalability")
+plt.show()
+
+
+hillfit,k,s,c,min_norm,max_norm = HILL_fit(d = data['c2_cume'],a = 1,epoch = 12000,init = 0, s = 1,k_perc = 3)
 plt.plot(de_normalize(hillfit,min_norm,max_norm))
-plt.plot(data['c1_cume'])
+plt.plot(data['c2_cume'])
+plt.show()
 
-
-
-proj_hil = de_normalize(hill_proj(k,s,[25,50,75,100,125,150,170,200,225,250,275,300],init = 25, c=c),min_norm,max_norm)
-plt.scatter([25,50,75,100,125,150,170,200,225,250,275,300],proj_hil)
-plt.plot(data['c1_cume'])
+proj_hil = de_normalize(hill_proj(k,s,[25,50,75,100,125,150,170,200,225,250,275,300],init = 0, c=c),min_norm,max_norm)
+plt.figure(figsize = (8,6))
+plt.scatter([25,50,75,100,125,150,170,200,225,250,275,300],proj_hil, label = 'Projection using Hill')
+plt.plot(data['c2_cume'], label = 'Cumulative KPI 2')
+plt.ylabel("Cumulative KPI")
+plt.xlabel("Time (Months)")
+plt.legend(loc = 2)
+plt.title("This Hill Projection May Indicate Further Scalability is Difficult")
+plt.show()
