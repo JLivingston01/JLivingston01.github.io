3688e5b24bf2285519f6996fd7a8dc3cf2873425
JasonLivingstonColumbia <33710575+JasonLivingstonColumbia@users.noreply.github.com>
2019-01-28 23:01:58 -0500

    edits

--- projects/hill_fit.py
+++ projects/hill_fit.py
@@ -3,12 +3,13 @@ import pandas as pd
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-
+#Load KPI data and calculate cumulative KPI
 data = pd.read_csv("path/to/data/company_data.csv")
 
 data['c1_cume'] = data['c1'].cumsum()
 data['c2_cume'] = data['c2'].cumsum()
 
+#Define Hill Function, Normilization and de_normilization, and the Hill Fitting Function
 def HILL(x,k,s,c):
     h = c/(1+(x/k)**(-s))
     return h
@@ -46,7 +47,6 @@ def HILL_fit(d,epoch = 200, a = .1,init = 0, s = 1.9, k_perc = 2, c_init = 1.3):
     
     return fit,k,s,c,min_norm,max_norm
 
-
 def hill_proj(k,s,p, c,init = 0):
     k = k
     s = s
@@ -56,11 +56,8 @@ def hill_proj(k,s,p, c,init = 0):
         projection.append(h)
     return(projection)
 
-hillfit,k,s,c,min_norm,max_norm = HILL_fit(d = data['c2_cume'],epoch = 12000,init = 0, s = 10,k_perc = 3)
-plt.plot(de_normalize(hillfit,min_norm,max_norm))
-plt.plot(data['c2_cume'])
 
-
+#Fit the Hill Function to Company 1 data
 hillfit,k,s,c,min_norm,max_norm = HILL_fit(d = data['c1_cume'],epoch = 12000,a = 1,init = 0, s = 17,k_perc = 1)
 plt.figure(figsize = (8,6))
 plt.plot(de_normalize(hillfit,min_norm,max_norm), label='Hill Fit')
@@ -71,7 +68,7 @@ plt.ylabel("Cumulative KPI")
 plt.xlabel("Time (Months)")
 plt.show()
 
-#plt.plot((de_normalize(hillfit,min_norm,max_norm) - data['c1_cume'])/data['c1_cume'])
+#Plot the magnitude error Monthly, check for near-zero centering
 mag_error = (de_normalize(hillfit,min_norm,max_norm) - data['c1_cume'])
 plt.figure(figsize = (8,6))
 plt.plot(mag_error)
@@ -80,7 +77,7 @@ plt.xlabel("Time (Months)")
 plt.title("Model Error Shows Normal Periodicity, Well Centered Near 0")
 plt.show()
 
-              
+#Project by Hill model and denormalize for Company 1, and plot against Company 1 KPI
 proj_hil = de_normalize(hill_proj(k,s,[25,50,75,100,125,150,170,200,225,250,275,300],init = 0, c=c),min_norm,max_norm)
 plt.figure(figsize = (8,6))
 plt.scatter([25,50,75,100,125,150,170,200,225,250,275,300],proj_hil, label = 'Projection using Hill')
@@ -91,7 +88,7 @@ plt.legend(loc = 2)
 plt.title("This Hill Projection May Indicate the Potential for Further Scalability")
 plt.show()
 
-
+#Calculate Hill Model for Company 2 KPI and Project
 hillfit,k,s,c,min_norm,max_norm = HILL_fit(d = data['c2_cume'],a = 1,epoch = 12000,init = 0, s = 1,k_perc = 3)
 plt.plot(de_normalize(hillfit,min_norm,max_norm))
 plt.plot(data['c2_cume'])
