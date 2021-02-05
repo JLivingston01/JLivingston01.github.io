ed405ac597f0658d1bc43f06ce6946984ea86cb7
JasonLivingstonColumbia <33710575+JasonLivingstonColumbia@users.noreply.github.com>
2021-02-04 22:53:38 -0500

    daily update

--- stock_signal.bat
+++ stock_signal.bat
@@ -1,5 +1,5 @@
-python "c:/users/jliv/Stock_Signal/pull_finance_data_v3.py"
-python "c:/users/jliv/Stock_Signal/app_v2.py"
+python "c:/users/jliv/JLivingston01.github.io/pull_finance_data_v3.py"
+python "c:/users/jliv/JLivingston01.github.io/app_v2.py"
 
 git commit --all -m "daily update"
 
