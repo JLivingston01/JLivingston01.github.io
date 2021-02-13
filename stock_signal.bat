7984cb747864e9eed42c657761de887b9c48d22b
JasonLivingstonColumbia <33710575+JasonLivingstonColumbia@users.noreply.github.com>
2021-02-13 15:51:37 -0500

    new puller

--- stock_signal.bat
+++ stock_signal.bat
@@ -1,4 +1,4 @@
-python "c:/users/jliv/Stock_Signal/pull_finance_data_v3.py"
+python "c:/users/jliv/Stock_Signal/pull_finance_data_v4.py"
 python "c:/users/jliv/Stock_Signal/app_v2.py"
 
 git commit --all -m "daily update"
