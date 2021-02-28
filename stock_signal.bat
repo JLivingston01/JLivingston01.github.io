d868cf630d6d26736378f8f869bdbd981266b129
JasonLivingstonColumbia <33710575+JasonLivingstonColumbia@users.noreply.github.com>
2021-02-28 17:27:59 -0500

    'table'

--- stock_signal.bat
+++ stock_signal.bat
@@ -1,5 +1,5 @@
 python "c:/users/jliv/Stock_Signal/pull_finance_data_v4.py"
-python "c:/users/jliv/Stock_Signal/app_v2.py"
+python "c:/users/jliv/Stock_Signal/app_v3.py"
 
 git commit --all -m "daily update"
 
